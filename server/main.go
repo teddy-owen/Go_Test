c04d84647e114002728f5f2f6a5c4f9496f2984b
Teddy Owen <david.e.owen1@gmail.com>
2019-01-31 20:27:29 -0500

    added CORS header

--- server/main.go
+++ server/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    enableCors(&w)
 	fmt.Println(r.URL)
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
+    enableCors(&w)
 	fmt.Println(r.URL.Path[1:])
     fmt.Fprintf(w, "<h1>Welcome to the about page!</h1>")
 }
@@ -20,4 +22,10 @@ func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/about/", about)
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
